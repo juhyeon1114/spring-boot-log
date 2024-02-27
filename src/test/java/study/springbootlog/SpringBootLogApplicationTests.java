6694f80bdab0930ce5a5e0815aff036ce4f8514f
juhyeon <juhyeon1114@gmail.com>
2024-02-27 12:50:23 +0900

    xml

--- src/test/java/study/springbootlog/SpringBootLogApplicationTests.java
+++ src/test/java/study/springbootlog/SpringBootLogApplicationTests.java
@@ -1,13 +1,20 @@
 package study.springbootlog;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class SpringBootLogApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("로깅 테스트")
+	void loggingTest() {
+		for (int i = 0; i < 10000; i++) {
+			log.error("공습경보 {}", i + 1);
+		}
 	}
 
 }
